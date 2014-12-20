f5bda617cfeda2a6b66ce29c48983910299e03e5
gaomeng <gaomengaz@163.com>
2014-12-20 16:15:49 +0800

    1
    1

--- YiYiProject/YiYiProject/PersonalApi/GMAPI/Gcontrollers/GMapViewController.h
+++ YiYiProject/YiYiProject/PersonalApi/GMAPI/Gcontrollers/GMapViewController.h
@@ -7,7 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BMapKit.h"
 
-@interface GMapViewController : UIViewController
+@interface GMapViewController : UIViewController<BMKMapViewDelegate,BMKLocationServiceDelegate>
+{
+    BMKMapView* _mapView;//地图
+    BMKLocationService* _locService;//定位服务
+    UIButton* _startBtn;//开始按钮
+    UIButton* _stopBtn;//结束按钮
+    UIButton* _followingBtn;//跟随模式
+    UIButton* _followHeadBtn;//罗盘模式
+    
+}
+
+///开始定位
+-(void)startLocation:(UIButton*)sender;
+
+///停止定位
+-(void)stopLocation:(UIButton*)sender;
+
+///跟随模式
+-(void)startFollowing:(UIButton*)sender;
+
+///罗盘模式
+-(void)startFollowHeading:(UIButton*)sender;
+
 
 @end
--- YiYiProject/YiYiProject/PersonalApi/GMAPI/Gcontrollers/GMapViewController.m
+++ YiYiProject/YiYiProject/PersonalApi/GMAPI/Gcontrollers/GMapViewController.m
@@ -9,121 +9,220 @@
 #import "GMapViewController.h"
 #import "BMapKit.h"
 
-@interface GMapViewController ()<BMKMapViewDelegate,BMKLocationServiceDelegate>
-{
-    BMKMapView *_mapView;
-    BMKLocationService *_locService;
-}
+@interface GMapViewController ()
+
 @end
 
 @implementation GMapViewController
 
 
 
-
--(void)viewWillAppear:(BOOL)animated {
-    [_mapView viewWillAppear];
-    _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
-    _locService.delegate = self;
+- (void)dealloc {
+    
+    
+    if (_mapView) {
+        _mapView = nil;
+    }
+    
+    if (_locService) {
+        _locService = nil;
+    }
 }
-
--(void)viewWillDisappear:(BOOL)animated {
-    [_mapView viewWillDisappear];
-    _mapView.delegate = nil; // 不用时，置nil
-    _locService.delegate = nil;
-}
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     //适配ios7
     if( ([[[UIDevice currentDevice] systemVersion] doubleValue]>=7.0))
     {
         self.navigationController.navigationBar.translucent = NO;
     }
-    _locService = [[BMKLocationService alloc]init];
-    _locService.delegate = self;
     
     
-    //定位
-    _locService = [[BMKLocationService alloc]init];
-    _locService.delegate = self;
-    [_locService startUserLocationService];//启动LocationService
+    [self setForBtn];
+    [self setMapAndLocationService];
     
-    [_locService startUserLocationService];
     
-    BMKMapView* mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, DEVICE_WIDTH, DEVICE_HEIGHT-44)];
-    _mapView.showsUserLocation = NO;//先关闭显示的定位图层
-    [_mapView setZoomLevel:17];// 设置地图级别
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [_mapView viewWillAppear];
+    
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    [_mapView viewWillDisappear];
+    
+    //停止定位
+    [_locService stopUserLocationService];
+    _mapView.showsUserLocation = NO;
+    
+    //代理置空
+    _mapView.delegate = nil;
+    _locService.delegate = nil;
+    
+    
+    
+}
+
+
+
+#pragma mark - 初始化地图和定位服务
+-(void)setMapAndLocationService{
+    _mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 44, DEVICE_WIDTH, DEVICE_HEIGHT-44)];
     _mapView.delegate = self;
-    _mapView.userTrackingMode = BMKUserTrackingModeFollow;//设置定位的状态
-    _mapView.showsUserLocation = YES;//显示定位图层
     
+    _locService = [[BMKLocationService alloc]init];
+    _locService.delegate = self;
     
-    self.view = mapView;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.view addSubview:_mapView];
 }
 
 
-/**
- *在地图View将要启动定位时，会调用此函数
- *@param mapView 地图View
- */
+#pragma mark - 按钮btn相关
+-(void)setForBtn{
+    _startBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _startBtn.backgroundColor = RGBCOLOR_ONE;
+    [_startBtn setTitle:@"开始" forState:UIControlStateNormal];
+    [_startBtn setFrame:CGRectMake(0, 0, DEVICE_WIDTH/4, 44)];
+    [_startBtn addTarget:self action:@selector(startLocation:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    _followingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _followingBtn.backgroundColor = RGBCOLOR_ONE;
+    [_followingBtn setTitle:@"跟随定位" forState:UIControlStateNormal];
+    [_followingBtn setFrame:CGRectMake(CGRectGetMaxX(_startBtn.frame), 0, DEVICE_WIDTH/4, 44)];
+    [_followingBtn addTarget:self action:@selector(startFollowing:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    _followHeadBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _followHeadBtn.backgroundColor = RGBCOLOR_ONE;
+    [_followHeadBtn setTitle:@"罗盘定位" forState:UIControlStateNormal];
+    [_followHeadBtn setFrame:CGRectMake(CGRectGetMaxX(_followingBtn.frame), 0, DEVICE_WIDTH/4, 44)];
+    [_followHeadBtn addTarget:self action:@selector(startFollowHeading:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _stopBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _stopBtn.backgroundColor = RGBCOLOR_ONE;
+    [_stopBtn setTitle:@"停止定位" forState:UIControlStateNormal];
+    [_stopBtn setFrame:CGRectMake(CGRectGetMaxX(_followHeadBtn.frame), 0, DEVICE_WIDTH/4, 44)];
+    [_stopBtn addTarget:self action:@selector(stopLocation:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [_followHeadBtn setEnabled:NO];
+    [_followingBtn setAlpha:0.6];
+    [_followingBtn setEnabled:NO];
+    [_followHeadBtn setAlpha:0.6];
+    [_stopBtn setEnabled:NO];
+    [_stopBtn setAlpha:0.6];
+    
+    [self.view addSubview:_startBtn];
+    [self.view addSubview:_followingBtn];
+    [self.view addSubview:_followHeadBtn];
+    [self.view addSubview:_stopBtn];
+    
+    
+}
+
+
+#pragma mark - 定位相关
+
+//在地图View将要启动定位时，会调用此函数
 - (void)willStartLocatingUser
 {
     NSLog(@"start locate");
 }
 
-/**
- *用户方向更新后，会调用此函数
- *@param userLocation 新的用户位置
- */
+//用户方向更新后，会调用此函数
 - (void)didUpdateUserHeading:(BMKUserLocation *)userLocation
 {
     [_mapView updateLocationData:userLocation];
     NSLog(@"heading is %@",userLocation.heading);
+    
+    
+    
+    
 }
 
-/**
- *用户位置更新后，会调用此函数
- *@param userLocation 新的用户位置
- */
+//用户位置更新后，会调用此函数
 - (void)didUpdateUserLocation:(BMKUserLocation *)userLocation
 {
-    //    NSLog(@"didUpdateUserLocation lat %f,long %f",userLocation.location.coordinate.latitude,userLocation.location.coordinate.longitude);
+    NSLog(@"didUpdateUserLocation lat %f,long %f",userLocation.location.coordinate.latitude,userLocation.location.coordinate.longitude);
     [_mapView updateLocationData:userLocation];
 }
 
-/**
- *在地图View停止定位后，会调用此函数
- *@param mapView 地图View
- */
+
+//在地图View停止定位后，会调用此函数
 - (void)didStopLocatingUser
 {
     NSLog(@"stop locate");
 }
 
-/**
- *定位失败后，会调用此函数
- *@param mapView 地图View
- *@param error 错误号，参考CLError.h中定义的错误号
- */
+//定位失败后，会调用此函数
 - (void)didFailToLocateUserWithError:(NSError *)error
 {
     NSLog(@"location error");
 }
 
 
-- (void)dealloc {
-    if (_mapView) {
-        _mapView = nil;
-    }
+
+#pragma mark - 按钮点击方法
+
+//普通态
+-(void)startLocation:(UIButton*)sender
+{
+    NSLog(@"进入普通定位态");
+    [_locService startUserLocationService];
+    _mapView.showsUserLocation = NO;//先关闭显示的定位图层
+    _mapView.userTrackingMode = BMKUserTrackingModeNone;//设置定位的状态
+    _mapView.showsUserLocation = YES;//显示定位图层
+    [_startBtn setEnabled:NO];
+    [_startBtn setAlpha:0.6];
+    [_stopBtn setEnabled:YES];
+    [_stopBtn setAlpha:1.0];
+    [_followHeadBtn setEnabled:YES];
+    [_followHeadBtn setAlpha:1.0];
+    [_followingBtn setEnabled:YES];
+    [_followingBtn setAlpha:1.0];
 }
+//罗盘态
+-(void)startFollowHeading:(UIButton*)sender
+{
+    NSLog(@"进入罗盘态");
+    
+    _mapView.showsUserLocation = NO;
+    _mapView.userTrackingMode = BMKUserTrackingModeFollowWithHeading;
+    _mapView.showsUserLocation = YES;
+    
+}
+//跟随态
+-(void)startFollowing:(UIButton*)sender
+{
+    NSLog(@"进入跟随态");
+    
+    _mapView.showsUserLocation = NO;
+    _mapView.userTrackingMode = BMKUserTrackingModeFollow;
+    _mapView.showsUserLocation = YES;
+    
+}
+//停止定位
+-(void)stopLocation:(UIButton*)sender
+{
+    [_locService stopUserLocationService];
+    _mapView.showsUserLocation = NO;
+    [_stopBtn setEnabled:NO];
+    [_stopBtn setAlpha:0.6];
+    [_followHeadBtn setEnabled:NO];
+    [_followHeadBtn setAlpha:0.6];
+    [_followingBtn setEnabled:NO];
+    [_followingBtn setAlpha:0.6];
+    [_startBtn setEnabled:YES];
+    [_startBtn setAlpha:1.0];
+}
+
+
+
+
+
+
+
 
 
 @end
